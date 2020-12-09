be1c8d700bcee1826e2a691a0af96d7ed2081e39
lockdef <locktki@icloud.com>
2020-12-09 11:41:40 +0900

    Headerを実装

---
+++ components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-white-bg flex justify-between items-center flex-d">
+    <h1 class="text-green-text px-4 m-3 text-3xl font-bold">ChatApp</h1>
+    <a
+      href="https://github.com/Lockdef/chatApp/issues"
+      class="bg-white-bg border-green-text font-bold border-solid border-2 rounded-2xl px-4 py-3 m-3 mr-8 text-md text-green-text"
+    >
+      Contact
+    </a>
+  </div>
+</template>
