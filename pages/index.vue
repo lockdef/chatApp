736505cbbb99dc26cdbc6eff56a462b8f8a9d21d
lockdef <locktki@icloud.com>
2020-12-10 23:59:34 +0900

    Footerを追加

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,12 @@
   <div class="bg-gradient-to-br from-green-bg to-purple-bg w-screen h-screen">
     <Header />
     <div class="pt-20 mx-auto max-w-4xl h-screen bg-white-blur">
-      <Message message="こんぺこー！！" :is-myself="true" />
-      <Message message="こんぺこー！！" :is-myself="false" />
-      <Message message="こんぺこー！！" :is-myself="true" />
+      <div class="flex flex-col">
+        <Message message="こんぺこー！！" :is-myself="true" />
+        <Message message="こんぺこー！！" :is-myself="false" />
+        <Message message="こんぺこー！！" :is-myself="true" />
+      </div>
+      <Footer />
     </div>
   </div>
 </template>
@@ -14,11 +17,13 @@ import Vue from 'vue'
 
 import Header from '@/components/Header.vue'
 import Message from '@/components/Message.vue'
+import Footer from '@/components/Footer.vue'
 
 export default Vue.extend({
   components: {
     Header,
     Message,
+    Footer,
   },
 })
 </script>
