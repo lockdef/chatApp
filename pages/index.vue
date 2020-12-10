67414c10fa7108c6ca5ba223db2d3106259a8f68
lockdef <locktki@icloud.com>
2020-12-10 19:18:48 +0900

    HeaderとLogoを読み込み

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,25 @@
 <template>
-  <div
-    class="bg-gradient-to-r from-green-bg to-purple-bg w-screen h-screen"
-  ></div>
+  <div class="bg-gradient-to-br from-green-bg to-purple-bg w-screen h-screen">
+    <Header />
+    <div class="pt-20 mx-auto max-w-4xl h-screen bg-white-blur">
+      <Message message="こんぺこー！！" :is-myself="true" />
+      <Message message="こんぺこー！！" :is-myself="false" />
+      <Message message="こんぺこー！！" :is-myself="true" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
-</script>
+import Header from '@/components/Header.vue'
+import Message from '@/components/Message.vue'
 
+export default Vue.extend({
+  components: {
+    Header,
+    Message,
+  },
+})
+</script>
 <style></style>
