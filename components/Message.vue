43aeff6abddd67a3c653228c0fde24fe8f89e96f
lockdef <locktki@icloud.com>
2020-12-10 19:19:10 +0900

    Message.vue大枠の実装

---
+++ components/Message.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div
+      :class="isMyself ? 'bg-green-main' : 'bg-white-bg'"
+      class="p-6 inline-block my-8 mx-"
+    >
+      <p class="text-black-text" v-text="message" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    /**
+     * メッセージ本文
+     */
+    message: {
+      required: true,
+      type: String,
+    },
+    /**
+     * ユーザー自身のメッセージかどうか
+     */
+    isMyself: {
+      required: true,
+      type: Boolean,
+    },
+  },
+})
+</script>
