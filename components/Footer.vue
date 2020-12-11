50614138b02f846f35477ce323b5aa147b629757
lockdef <locktki@icloud.com>
2020-12-11 20:57:57 +0900

    送信ボタンを追加

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="flex absolute bottom-0 max-w-4xl w-full bg-white-bg">
+  <div
+    class="flex justify-between absolute bottom-0 max-w-4xl w-full bg-white-bg"
+  >
     <textarea
       v-model="message"
-      class="bg-gray-input w-180 h-16 px-6 py-4 lg:ml-12 ml-4 my-6 rounded-full resize-none text-lg text-black-text focus:outline-none focus:ring focus:border-green-main focus:ring-offset-green-main"
+      placeholder="新しいメッセージを作成する"
+      class="bg-gray-input w-full h-16 px-6 py-4 lg:ml-12 ml-4 my-6 rounded-full resize-none text-lg text-black-text boder-solid border-green-main focus:bg-white-bg focus:outline-none focus:ring focus:ring-green-main"
       cols="1"
     />
-    <fa-icon icon="paper-plane" class="h-16 text-green-main" />
+    <button
+      :class="{ 'opacity-60 cursor-default': message.length === 0 }"
+      class="text-green-text text-right rounded-full focus:outline-none my-4 mr-4 ml-3 lg:mr-12"
+    >
+      <font-awesome-icon
+        :icon="['far', 'paper-plane']"
+        :class="{ 'hover:bg-green-hover': message.length > 0 }"
+        class="text-6xl p-3 rounded-full"
+      />
+    </button>
   </div>
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import { Chat } from '@/entities/chat'
+import CreateChatUseCase from '@/usecase/chat/createChatUseCase'
+
+const createChatUseCase = new CreateChatUseCase()
 
 type LocalState = {
   message: string
@@ -21,5 +37,12 @@ export default Vue.extend({
       message: '',
     }
   },
+  methods() {
+    createChat() {
+      const dto: Chat = {
+
+      }
+    }
+  }
 })
 </script>
