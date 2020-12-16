9ca22b88c40a5104a53e913f15ab01156b9a8e7f
lockdef <locktki@icloud.com>
2020-12-16 18:46:22 +0900

    ctrl+Enterで送信する実装

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,6 +7,7 @@
       placeholder="新しいメッセージを作成する"
       class="scrollbar-none bg-gray-input w-full h-16 px-6 py-4 lg:ml-12 ml-4 my-6 rounded-full resize-none text-lg text-black-text boder-solid border-green-main focus:bg-white-bg focus:outline-none focus:ring focus:ring-green-main"
       cols="1"
+      @keydown.enter.ctrl="createChat"
     />
     <button
       :class="{ 'opacity-60 cursor-default': message.length === 0 }"
