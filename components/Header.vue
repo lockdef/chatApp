f10b86cdfee35c7af7ec88e15d1183a7c03dffda
lockdef <locktki@icloud.com>
2020-12-17 18:31:42 +0900

    ContactをAboutに、リンク先をNotionに変更

--- components/Header.vue
+++ components/Header.vue
@@ -2,12 +2,14 @@
   <div
     class="absolute top-0 w-screen bg-white-bg flex justify-between items-center flex-d h-20"
   >
-    <h1 class="text-green-text px-4 m-3 text-3xl font-bold">ChatApp</h1>
-    <a
-      href="https://github.com/Lockdef/chatApp/issues"
-      class="bg-white-bg border-green-text font-bold border-solid border-2 rounded-2xl px-4 py-3 m-3 mr-8 text-md text-green-text"
-    >
-      Contact
-    </a>
+    <h1 class="text-green-text px-4 m-3 mr-0 text-3xl font-bold">ChatApp</h1>
+    <div>
+      <a
+        href="https://www.notion.so/ChatApp-3c87cc033e454e0da9590eb5e8b23cc3"
+        class="bg-white-bg border-green-text font-bold border-solid border-2 rounded-2xl px-4 py-3 m-3 mr-8 text-md text-green-text"
+      >
+        About
+      </a>
+    </div>
   </div>
 </template>
